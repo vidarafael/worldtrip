e800321e6d2a58fbe6fd502e5f727ff231fa5d3c
Rafael Freitas <vidarafael@hotmail.com>
2021-10-13 22:41:37 -0300

    adicionando swiper

--- src/components/Slide.tsx
+++ src/components/Slide.tsx
@@ -1,22 +1,37 @@
+import { Navigation, Pagination, A11y } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { Image } from '@chakra-ui/image'
+import { Box } from '@chakra-ui/layout';
+
 
 // Import Swiper styles
 import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
 
 const Slide = (): JSX.Element => {
   return (
-    <Swiper
-      spaceBetween={50}
-      slidesPerView={3}
-      onSlideChange={() => console.log('slide change')}
-      onSwiper={(swiper) => console.log(swiper)}
-    >
-      <SwiperSlide>Slide 1</SwiperSlide>
-      <SwiperSlide>Slide 2</SwiperSlide>
-      <SwiperSlide>Slide 3</SwiperSlide>
-      <SwiperSlide>Slide 4</SwiperSlide>
-      ...
-    </Swiper>
+    <Box maxWidth="1080px" mx="auto" my="30px">
+      <Swiper
+      // install Swiper modules
+        modules={[Navigation, Pagination, A11y]}
+        spaceBetween={10}
+        slidesPerView={1}
+        navigation
+        pagination={{ clickable: true }}
+        scrollbar={{ draggable: true }}
+        onSwiper={(swiper) => console.log(swiper)}
+        onSlideChange={() => console.log('slide change')}
+      >
+        <SwiperSlide>
+          <Image src="/images/continents/europacontinent.png" alt="logo" />
+        </SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+      </Swiper>
+    </Box>
   )
 }
 
