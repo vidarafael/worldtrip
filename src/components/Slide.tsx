c68017e707b17f097514c623a3003a2015ca9fbb
Rafael Freitas <vidarafael@hotmail.com>
2021-10-17 11:38:04 -0300

    adicionando estilização para o swiper

--- src/components/Slide.tsx
+++ src/components/Slide.tsx
@@ -8,38 +8,58 @@ import { Box } from '@chakra-ui/layout';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
-import styles from '../styles/styles.module.css';
 
 
 const Slide = (): JSX.Element => {
+  const css = `
+    div.swiper-button-prev,
+    div.swiper-button-next {
+      color: #FFBA08;
+    }
+
+    span.swiper-pagination-bullet {
+      background: #999999;
+      opacity: 1;
+    }
+
+    span.swiper-pagination-bullet-active {
+      background: #FFBA08;
+    }
+
+  `
+
   return (
-    <Box maxWidth="1080px" mx="auto" my="30px">
-      <Swiper
-      // install Swiper modules
-        className={styles['swiper-button-prev']}
-        modules={[Navigation, Pagination, A11y]}
-        spaceBetween={10}
-        slidesPerView={1}
-        navigation
-        pagination={{ clickable: true }}
-        scrollbar={{ draggable: true }}
-        onSwiper={(swiper) => console.log(swiper)}
-        onSlideChange={() => console.log('slide change')}
-      >
-        <SwiperSlide>
-          <Image src="/images/continents/europacontinent.png" alt="logo" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <Image src="/images/continents/europacontinent.png" alt="logo" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <Image src="/images/continents/europacontinent.png" alt="logo" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <Image src="/images/continents/europacontinent.png" alt="logo" />
-        </SwiperSlide>
-      </Swiper>
-    </Box>
+    <>
+      <style>
+        {css}
+      </style>
+      <Box maxWidth="1080px" mx="auto" my="30px">
+        <Swiper
+        // install Swiper modules
+          modules={[Navigation, Pagination, A11y]}
+          spaceBetween={10}
+          slidesPerView={1}
+          navigation
+          pagination={{ clickable: true }}
+          scrollbar={{ draggable: true }}
+          onSwiper={(swiper) => console.log(swiper)}
+          onSlideChange={() => console.log('slide change')}
+        >
+          <SwiperSlide>
+            <Image src="/images/continents/europacontinent.png" alt="logo" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Image src="/images/continents/europacontinent.png" alt="logo" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Image src="/images/continents/europacontinent.png" alt="logo" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Image src="/images/continents/europacontinent.png" alt="logo" />
+          </SwiperSlide>
+        </Swiper>
+      </Box>
+    </>
   )
 }
 
