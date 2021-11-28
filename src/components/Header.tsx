4cb7027d583217652be9160784606abc5b48241a
Rafael Freitas <vidarafael@hotmail.com>
2021-11-28 18:37:30 -0300

    add header in page continent

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,26 @@
-import { Flex } from '@chakra-ui/layout'
+import { Box, Flex, Link } from '@chakra-ui/layout'
 import { Image } from '@chakra-ui/image'
+import { AiOutlineLeft } from "react-icons/ai";
 
+interface IHeaderProps {
+  isBackHome?: boolean
+}
 
-const Header = () => {
-  return (
+const Header = ({isBackHome}: IHeaderProps) => {
+  return isBackHome ? (
+    <header>
+        <Flex
+          align="center"
+          justify="space-between"
+          py="5"
+          px={[5, "100px"]}
+        >
+          <Link href="/"><AiOutlineLeft size={24}/></Link>
+          <Image src="/images/logo.svg" alt="logo" />
+          <Box></Box>
+        </Flex>
+      </header>
+  ) : (
     <header>
         <Flex
           justify="center"
